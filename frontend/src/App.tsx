38d77094dcdfffbfee85112fdfcffe625602d3fe
AndreBrandao88 <andretuska@gmail.com>
2021-09-08 23:23:25 -0300

    Atualização

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,24 +8,24 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className = "container">
+      <div className="container">
         <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
 
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className ="text-center text-secondary"> Taxa de sucesso(%)</h5>
+            <h5 className="text-center text-secondary"> Taxa de sucesso(%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
             <h5> Todas vendas</h5>
             <DonutChart />
           </div>
-        <div className ="py-3">
+          <div className="py-3">
 
-          <h2 className ="text-primary"> Todas as vendas 
+            <h2 className="text-primary"> Todas as vendas
 
-          </h2>
-        </div>
+            </h2>
+          </div>
 
         </div>
 
@@ -34,7 +34,7 @@ function App() {
       </div>
       <Footer />
 
-      
+
     </>
   );
 }
