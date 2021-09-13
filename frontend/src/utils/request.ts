0b6512a225d456b4cd944400de65936c503abc74
AndreBrandao88 <andretuska@gmail.com>
2021-09-12 21:50:16 -0300

    trying fix 4

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds4-geny.herokuapp.com/';
